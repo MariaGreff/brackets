35bf074d0ae120e344c2bd7dcb97bf7f33c14ea1
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-03-22 16:14:35 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr = bracketsConfig.toString().split(",");
+    let newArr = arr
+        .map((j, i) => arr[i].concat(arr[i + 1]))
+        .filter((j, i) => (i % 2 == 0 ? arr[i] : ""));
+    let index = 0;
+    do {
+        str = str.replace(newArr[index], "");
+        index = newArr.findIndex((j, i) => str.indexOf(j) != -1);
+    } while (index != -1);
+    return str.length == 0;
+};
